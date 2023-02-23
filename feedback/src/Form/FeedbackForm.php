738fbbc9d548703d49d6f21e3ad23bb2c8c7a4b3
Wayne Eaker <wayne@zengenuity.com>
2023-02-23 15:34:03 -0500

    Add customizations to the feedback form to implement the requirements for access to fields and validation

--- feedback/src/Form/FeedbackForm.php
+++ feedback/src/Form/FeedbackForm.php
@@ -13,6 +13,35 @@ class FeedbackForm extends ContentEntityForm {
   /**
    * {@inheritdoc}
    */
+  public function form(array $form, FormStateInterface $form_state) {
+    $form = parent::form($form, $form_state);
+    $form['revision']['#default_value'] = TRUE;
+    if (!\Drupal::currentUser()->hasPermission('edit feedback')) {
+      $form['uid']['#access'] = FALSE;
+      $form['created']['#access'] = FALSE;
+      $form['status']['#access'] = FALSE;
+      $form['revision_log']['#access'] = FALSE;
+    }
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+    $address = $form_state->getValue('address');
+    if (!str_contains($address[0]['value'], 'Springfield')) {
+      $form_state->setError(
+        $form['address'],
+        $this->t("We're sorry. We can only service requests from Springfield.")
+      );
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function save(array $form, FormStateInterface $form_state) {
     $result = parent::save($form, $form_state);
 
@@ -26,8 +55,11 @@ class FeedbackForm extends ContentEntityForm {
 
     switch ($result) {
       case SAVED_NEW:
-        $this->messenger()->addStatus($this->t('New feedback %label has been created.', $message_arguments));
+        $this->messenger()->addStatus(
+          $this->t('Thank you for submitting your feedback. You should receive a response in 5 - 10 business days.')
+        );
         $this->logger('feedback')->notice('Created new feedback %label', $logger_arguments);
+
         break;
 
       case SAVED_UPDATED:
@@ -36,7 +68,12 @@ class FeedbackForm extends ContentEntityForm {
         break;
     }
 
-    $form_state->setRedirect('entity.feedback.canonical', ['feedback' => $entity->id()]);
+    if (!\Drupal::currentUser()->hasPermission('view feedback')) {
+      $form_state->setRedirect('<front>');
+    }
+    else {
+      $form_state->setRedirect('entity.feedback.canonical', ['feedback' => $entity->id()]);
+    }
 
     return $result;
   }
